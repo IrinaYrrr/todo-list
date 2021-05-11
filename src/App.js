0e2d46f8c26871e8be5e7e2199b169a253f8de79
Irina <ioblomova13@gmail.com>
2021-05-11 19:33:21 +0300

    update changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Alert } from './components/Alert';
 import { Navbar } from './components/Navbar';
+import { AlertState } from './context/alert/AlertState';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4">
-        <Alert />
-        <Switch>
-          <Route path={'/'} exact component={Home} />
-          <Route path={'/about'} component={About} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert />
+          <Switch>
+            <Route path={'/'} exact component={Home} />
+            <Route path={'/about'} component={About} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
